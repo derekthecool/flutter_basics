029b34163df609d6c8a8abd0ec39567d84df9781
Derek Lomax <derekthecool@gmail.com>
2024-02-06 11:26:07 -0700

    Work on basics again

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Add this to center Row content
+            children: <Widget>[
+              Text("Test text"),
+              Text("tester tester"),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text("This is a column"),
+                  Text("next column"),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     ),
   );
