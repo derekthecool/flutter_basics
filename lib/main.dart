8b2d78b9e8a33617918f266dfc210119d8e3ca3b
Derek Lomax <derekthecool@gmail.com>
2024-02-07 21:40:19 -0700

    Add startings to chuck Norris joke application

--- lib/main.dart
+++ lib/main.dart
@@ -10,155 +10,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Chuck Norris Joke Generator',
       darkTheme: ThemeData(
         brightness: Brightness.dark,
-        colorSchemeSeed: Colors.grey,
-      ),
-      theme: ThemeData(
-        colorSchemeSeed: Colors.grey,
+        colorSchemeSeed: Colors.green,
         useMaterial3: true,
       ),
-      themeMode: ThemeMode.dark,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(
+        colorSchemeSeed: Colors.green,
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
+      home: ChuckView(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  void _updateMathString(String number) {
-    setState(() {
-      mathString += number;
-    });
-  }
-
-  String mathString = "";
+class _ChuckViewState extends State<ChuckView> {
+  // State variables items here
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Wrap(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      mathString,
-                      style: TextStyle(
-                        fontSize: 25,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                            flex: 1,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Icon(Icons.medical_services_rounded, size: 30),
-                                Icon(Icons.calculate_outlined, size: 30),
-                                Icon(
-                                  Icons.history,
-                                  size: 30,
-                                ),
-                              ],
-                            )),
-                        Expanded(
-                          flex: 1,
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Icon(Icons.phonelink_erase, size: 30),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Divider(
-                    height: 50,
-                  ),
-                  // Row 1
-                  CalculatorButton(number: "C", onPressed: _updateMathString),
-                  CalculatorButton(number: "()", onPressed: _updateMathString),
-                  CalculatorButton(number: "%", onPressed: _updateMathString),
-                  CalculatorButton(number: "/", onPressed: _updateMathString),
-
-                  // Row 2
-                  CalculatorButton(number: "7", onPressed: _updateMathString),
-                  CalculatorButton(number: "8", onPressed: _updateMathString),
-                  CalculatorButton(number: "9", onPressed: _updateMathString),
-                  CalculatorButton(number: "X", onPressed: _updateMathString),
-
-                  // Row 3
-                  CalculatorButton(number: "4", onPressed: _updateMathString),
-                  CalculatorButton(number: "6", onPressed: _updateMathString),
-                  CalculatorButton(number: "5", onPressed: _updateMathString),
-                  CalculatorButton(number: "-", onPressed: _updateMathString),
-
-                  // Row 4
-                  CalculatorButton(number: "1", onPressed: _updateMathString),
-                  CalculatorButton(number: "2", onPressed: _updateMathString),
-                  CalculatorButton(number: "3", onPressed: _updateMathString),
-                  CalculatorButton(number: "+", onPressed: _updateMathString),
-
-                  // Row 5
-                  CalculatorButton(number: "+-", onPressed: _updateMathString),
-                  CalculatorButton(number: "0", onPressed: _updateMathString),
-                  CalculatorButton(number: ".", onPressed: _updateMathString),
-                  CalculatorButton(number: "=", onPressed: _updateMathString),
-                ],
-              ),
-            ],
-          ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Chuck Norris Jokes'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Getting new Chuck Norris joke');
+        },
+        tooltip: 'Get new joke',
+        child: const Icon(Icons.add),
+      ),
     );
-    ;
   }
 }
 
-class CalculatorButton extends StatelessWidget {
-  const CalculatorButton({
-    super.key,
-    required this.number,
-    required this.onPressed,
-  });
-
-  final String number;
-  final Function(String) onPressed;
-
+class ChuckView extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-        width: 100,
-        height: 100,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Theme.of(context).colorScheme.onPrimaryContainer,
-        ),
-        child: ElevatedButton(
-          onPressed: () {
-            onPressed(number);
-          },
-          child: Text(
-            "$number",
-            style: TextStyle(fontSize: 30),
-          ),
-        ));
-  }
+  State<ChuckView> createState() => _ChuckViewState();
 }
