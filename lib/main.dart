2718acbc18d4e7041d7613295b3fd2f30aa279ea
Derek Lomax <derekthecool@gmail.com>
2024-02-08 09:43:18 -0700

    Add starter version of HTTP get for chuck Norris jokes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
+
+Future<void> fetchAndDecodeJson(String inputURL) async {
+  final url = Uri.parse(inputURL);
+
+  try {
+    // Creating a HttpClient
+    final httpClient = HttpClient();
+
+    // Making the HTTP GET request
+    final request =
+        await httpClient.getUrl(url); // Use getUrl to make a GET request
+    final response =
+        await request.close(); // Close the request and get the response
+
+    // Checking if the request was successful (status code 200)
+    if (response.statusCode == 200) {
+      // Reading the response content
+      final jsonString = await response.transform(utf8.decoder).join();
+      // Decoding the JSON
+      final data = jsonDecode(jsonString);
+
+      // Use the decoded data
+      print(data);
+    } else {
+      // Handling error, if the server does not return a status code of 200
+      print('Failed to load data. Status code: ${response.statusCode}');
+    }
+
+    // Closing the HttpClient to free system resources
+    httpClient.close();
+  } catch (e) {
+    // Handling any errors that occur during the request
+    print('Error: $e');
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -45,6 +82,7 @@ class _ChuckViewState extends State<ChuckView> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           print('Getting new Chuck Norris joke');
+          fetchAndDecodeJson('https://api.chucknorris.io/jokes/random');
         },
         tooltip: 'Get new joke',
         child: const Icon(Icons.add),
